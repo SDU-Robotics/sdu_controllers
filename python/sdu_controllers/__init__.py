87fb1c20ea9fbf74a6d7db7dfa88cd760f7e6ee5
Carolin Nowak <cano@mmmi.sdu.dk>
2025-01-13 15:47:46 +0100

    Added python bindings and example for operational space controller.py

--- python/sdu_controllers/__init__.py
+++ python/sdu_controllers/__init__.py
@@ -3,9 +3,12 @@ from sdu_controllers._sdu_controllers import RobotType
 from sdu_controllers._sdu_controllers import URRobotModel
 from sdu_controllers._sdu_controllers import BreedingBlanketHandlingRobotModel
 from sdu_controllers._sdu_controllers import PDController
+from sdu_controllers._sdu_controllers import OperationalSpaceController
 from sdu_controllers._sdu_controllers import AdmittanceControllerPosition
 from sdu_controllers._sdu_controllers import InverseDynamicsJointSpace
 from sdu_controllers._sdu_controllers import ForwardDynamics
+from sdu_controllers._sdu_controllers import forward_kinematics
+
 
 # Export the version given in project metadata
 from importlib import metadata
