172cc45e5a42863a77a1916280528eb252873b86
Emil Lykke Diget <emil@lykke-diget.dk>
2025-06-06 14:28:38 +0200

    Updated simulink library.

--- simulink/tests/init.m
+++ simulink/tests/init.m
@@ -12,3 +12,6 @@ addpath("../library/")
 % Create robot model
 sdu_controllers_lib = py.importlib.import_module('sdu_controllers')
 bbrobot = sdu_controllers_lib.models.BreedingBlanketHandlingRobotModel();
+
+ur3e = sdu_controllers_lib.models.URRobotModel( ...
+    sdu_controllers_lib.models.RobotType(0));
