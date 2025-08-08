b818bd47fbf8ac0ff3fd4c21bd450941675b0df6
kasper Høj Lorenzen <kalor@mmmi.sdu.dk>
2025-08-08 09:45:10 +0200

    removed copy error

--- cmake/sdu_controllersConfig.cmake
+++ cmake/sdu_controllersConfig.cmake
@@ -1,6 +1,6 @@
 include("${CMAKE_CURRENT_LIST_DIR}/sdu_controllers-config.cmake")
 
-set(SDU_CONTROLLERS_LIBRARIES ML::MachineLearning)
+set(SDU_CONTROLLERS_LIBRARIES sdu_controllers::sdu_controllers)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(sdu_controllers DEFAULT_MSG SDU_CONTROLLERS_LIBRARIES)
