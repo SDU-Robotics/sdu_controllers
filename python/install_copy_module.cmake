5654efb54527ecffc316f25fff9d4ad3664c7622
Anders Prier Lindvig <anpl@mmmi.sdu.dk>
2026-05-11 13:51:52 +0200

    Updated path of copy_module script to use , attempt to fix windows problem.

--- python/install_copy_module.cmake
+++ python/install_copy_module.cmake
@@ -1,11 +1,19 @@
 # CMake script to copy the module during installation
 # This is executed during the install step on Windows
+# Variables available from CMake during install: CMAKE_INSTALL_PREFIX, CMAKE_INSTALL_CONFIG_NAME
 
-execute_process(
-  COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/copy_module.py ${CMAKE_INSTALL_PREFIX}
-  RESULT_VARIABLE copy_result
-)
+set(COPY_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/copy_module.py")
+set(INSTALL_DIR "${CMAKE_INSTALL_PREFIX}")
 
-if(NOT copy_result EQUAL 0)
-  message(WARNING "Failed to copy module to standard name during installation")
+if(NOT EXISTS "${COPY_SCRIPT}")
+  message(WARNING "copy_module.py not found at ${COPY_SCRIPT}")
+else()
+  execute_process(
+    COMMAND ${Python3_EXECUTABLE} "${COPY_SCRIPT}" "${INSTALL_DIR}"
+    RESULT_VARIABLE copy_result
+  )
+  
+  if(NOT copy_result EQUAL 0)
+    message(WARNING "Failed to copy module to standard name during installation")
+  endif()
 endif()
