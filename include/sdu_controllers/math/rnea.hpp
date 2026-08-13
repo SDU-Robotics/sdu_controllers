abfa178eddc378bf4550bc160479f6f61d6dd611
Mads Elkjær Larsen <madla@mmmi.sdu.dk>
2026-08-13 13:59:13 +0200

    fixed header

--- include/sdu_controllers/math/rnea.hpp
+++ include/sdu_controllers/math/rnea.hpp
@@ -30,9 +30,9 @@ namespace sdu_controllers::math
       Eigen::VectorXd velocity_product(const Eigen::VectorXd &q, const Eigen::VectorXd &dq);
       Eigen::VectorXd gravity(const Eigen::VectorXd &q);
       
-      // Get complete joint dynamics of every frame component
-      const Eigen::Matrix3Xd& get_joint_forces() const { return f_; }
-      const Eigen::Matrix3Xd& get_joint_moments() const { return mu_; }
+      // Get complete reaction dynamics of every joint/frame
+      const Eigen::Matrix3Xd& get_reaction_forces_frames() const { return f_; }
+      const Eigen::Matrix3Xd& get_reaction_moments_frames() const { return mu_; }
 
       /**
        * @brief Classical joint friction torque \f$ \tau_{f} \f$.
